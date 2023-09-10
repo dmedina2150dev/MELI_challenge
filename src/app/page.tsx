f6d6a9afdbc55b5ceab1e722ec2542dd0f2f2f69
Dajan Medina <dmedina2150dev@gmail.com>
2023-09-10 20:00:57 -0300

    [feat]: Empty home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Navbar from '@/components/Navbar'
-import Image from 'next/image'
-
+import ArrowUpIcon from '@/components/icons/ArrowUp'
 
 export default function Home() {
   return (
-    <header className="bg-amber-300 fixed w-full">
-      <Navbar />
-    </header>
-
+    <section className='min-h-screen -mt-24 grid place-content-center'>
+      <div className='flex flex-col items-center gap-5 text-2xl text-slate-300'>
+        <span className='animate-bounce'>
+          <ArrowUpIcon />
+        </span>
+        <h1>Consiga los mejores productos</h1>
+      </div>
+    </section>
   )
 }
