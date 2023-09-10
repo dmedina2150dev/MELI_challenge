55df9df6abf7f77a3684a4fd99849fa20e427373
Dajan Medina <dmedina2150dev@gmail.com>
2023-09-10 20:31:35 -0300

    [changes] texts home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import ArrowUpIcon from '@/components/icons/ArrowUp'
 export default function Home() {
   return (
     <section className='min-h-screen -mt-24 grid place-content-center'>
-      <div className='flex flex-col items-center gap-5 text-2xl text-slate-300'>
+      <div className='flex flex-col items-center gap-5 text-center text-xl  sm:text-2xl text-slate-300'>
         <span className='animate-bounce'>
           <ArrowUpIcon />
         </span>
-        <h1>Consiga los mejores productos</h1>
+        <h2>Consigue las mejores ofertas y productos</h2>
       </div>
     </section>
   )
