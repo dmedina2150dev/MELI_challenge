0e4b8e81e0c9b2a364e1916743c76eb8055a5e3d
Dajan Medina <dmedina2150dev@gmail.com>
2023-09-13 01:35:54 -0300

    [fix]: missing data when sorting

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -7,17 +7,19 @@ interface Props {
 
 export const useProducts = ({ searchsProducts }: Props): Product[] => {
 
+    console.log({searchsProducts})
+
     const products: Product[] = searchsProducts.map( product => ({
         id: product.id,
         title: product.title,
         price: {
-            currency: product.installments.currency_id,
+            currency: product.installments?.currency_id || '$',
             amount: product.price.toString(),
-            decimals: product.installments.rate
+            decimals: product.installments?.rate || 0
         },
         installments: {
-            quantity: product.installments.quantity,
-            amount: product.installments.amount.toString()
+            quantity: product.installments?.quantity || 1,
+            amount: product.installments?.amount.toString() || product.price.toString()
         },
         address: {
             state_name: product.address.state_name,
