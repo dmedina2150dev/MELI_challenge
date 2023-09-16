b234fe5816949baabcf396e8ff5cc8669f5b48b5
Dajan Medina <dmedina2150dev@gmail.com>
2023-09-15 21:35:41 -0300

    [refactor]: styles for mobile menu filter

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -23,11 +23,11 @@ export function Providers ({ children }: ProvidersProps) {
       <main className='mt-24 mb-24'>
         {children}
       </main>
-      <div className={`w-full fixed bottom-0 shadow-md border-t-2 rounded-t-xl text-gray-200 block sm:block md:hidden transition-all ease-in-out ${(showMenu) ? 'h-64' : 'h-14'}`}>
+      <div className={`w-full fixed bottom-0 shadow-md shadow-black border-t-2 rounded-t-xl bg-white text-gray-200 block sm:block md:hidden transition-all ease-in-out ${(showMenu) ? 'h-64' : 'h-14'}`}>
         <div className='flex flex-col justify-center'>
-          <button onClick={toggleMenu} className='mx-auto text-base text-slate-400 py-4 px-2 border-'>
+          <span onClick={toggleMenu} className='mx-auto text-base text-slate-400 py-4 px-2 active:bg-white target:bg-white focus:bg-white'>
             Ordenar / Filtrar
-          </button>
+          </span>
         </div>
         <div className='flex flex-col justify-center'>
           <FooterOptions />
